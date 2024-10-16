552f1e23f67cb7dc3b980a8295c3c893aa8ff39d
chong <wangchong@192.168.5.5>
2024-10-16 19:23:35 +0800

    Rollback

--- src/util/quillConfig.ts
+++ src/util/quillConfig.ts
@@ -1,34 +1,3 @@
-import Quill from "quill";
-
-// 定义 Inline 的类型
-interface InlineBlot {
-  new (): any;
-  create(value: string): Node;
-  formats(node: HTMLElement): any;
-}
-
-// 使用类型断言
-const Inline = Quill.import("blots/inline") as unknown as InlineBlot;
-
-class TagBlot extends (Inline as any) {
-  static create(value: string): HTMLElement {
-    let node = super.create(value) as HTMLElement;
-    node.setAttribute("data-tag", value);
-    node.textContent = `#${value}`;
-    return node;
-  }
-
-  static formats(node: HTMLElement): string | undefined {
-    return node.getAttribute("data-tag") || undefined;
-  }
-
-  static blotName = "tag";
-  static tagName = "span";
-  static className = "ql-tag";
-}
-
-Quill.register("formats/tag", TagBlot);
-
 // 配置 quill modules
 const modules = {
   toolbar: [
@@ -40,12 +9,10 @@ const modules = {
       "image",
       "clean",
     ],
-    ["tag"],
   ],
-  tagModule: true,
 };
 
 // 配置 quill formats
-const formats = ["list", "bold", "underline", "image", "tag"];
+const formats: Array<string> = ["list", "bold", "underline", "image"];
 
 export { modules, formats };
